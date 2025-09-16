4264f0b2014732a0d50a8a2986aa149535241926
HarryMolla <hmiol5577@gmail.com>
2025-09-16 18:08:16 +0300

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='p-20 bg-gray-400 flex items-center w-full'>App</div>
+  )
+}
+
+export default App
---
+++ src/Components.jsx/Catagroys.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Catagroys = () => {
+  return (
+    <div>Catagroys</div>
+  )
+}
+
+export default Catagroys
